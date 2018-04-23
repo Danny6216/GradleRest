696c34cd4993a3e78ccfcf24fde7a5e69bafae2d
Danny6216 <dann6216@gmail.com>
2018-04-23 16:58:47 +0300

    Update Application.java

--- src/main/java/App/Application.java
+++ src/main/java/App/Application.java
@@ -3,6 +3,8 @@ package App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//http://www.baeldung.com/jackson-bidirectional-relationships-and-infinite-recursion
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
